5a1e6ac98670b6a45c5e0c1965656cd9f17d720a
Aprajita Sharma <126262128+Aprajita-byte@users.noreply.github.com>
2025-01-29 22:58:26 +0530

    Add files via upload

---
+++ .next/static/webpack/webpack.6c194899a32158c1.hot-update.js
@@ -0,0 +1,10 @@
+
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "90c2b857c44992e0"; }
+/******/ }();
+/******/ 
+/******/ }
+);
---
+++ .next/static/webpack/webpack.90c2b857c44992e0.hot-update.js
@@ -0,0 +1,10 @@
+
+self["webpackHotUpdate_N_E"]("webpack",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "6ffdf9fa1ecdd686"; }
+/******/ }();
+/******/ 
+/******/ }
+);
